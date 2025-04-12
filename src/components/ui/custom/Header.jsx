58322f21bb10669c510a4cd4fdff7649b1879867
rishabh8911 <rsbh1125@gmail.com>
2025-04-12 12:20:25 +0530

    update2

--- src/components/ui/custom/Header.jsx
+++ src/components/ui/custom/Header.jsx
@@ -1,9 +1,8 @@
-import React, { useEffect ,useState} from "react";
+import React, { useEffect, useState } from "react";
 import { googleLogout } from "@react-oauth/google";
 import { useNavigate } from "react-router";
 import { useGoogleLogin } from "@react-oauth/google";
-import { Button } from "@/components/ui/Button";
-
+import { Button } from "@/components/ui/button";
 import { FcGoogle } from "react-icons/fc";
 import { HiHome } from "react-icons/hi2";
 
@@ -15,96 +14,106 @@ import {
   DialogHeader,
 } from "@/components/ui/dialog";
 
-
 function Header() {
-    const [user, setUser]= useState(null);
-    const [openDailog,setOpenDailog] = useState(false)
-//   const user = JSON.stringify(localStorage.getItem("user"));
+  const [user, setUser] = useState(null);
+  const [openDailog, setOpenDailog] = useState(false);
+  //   const user = JSON.stringify(localStorage.getItem("user"));
 
-useEffect(()=>{
+  useEffect(() => {
     const storedUser = localStorage.getItem("user");
-    if(storedUser){
-        setUser(JSON.parse(storedUser));
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
     }
-},[]);
+  }, []);
 
-const login = useGoogleLogin({
-    onSuccess:(codeResp)=>{
-      console.log("coderesponse",codeResp);
+  const login = useGoogleLogin({
+    onSuccess: (codeResp) => {
+      console.log("coderesponse", codeResp);
       GetUserProfile(codeResp);
-
     },
-    onError:(error)=>console.log("google login error",error)
-    
-    
-  })
+    onError: (error) => console.log("google login error", error),
+  });
 
-   const GetUserProfile=(tokenInfo)=>{
-      console.log("token info",tokenInfo);
-      
-      axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${tokenInfo?.access_token}`,{
-        headers:{
-          Authorization:`Bearer ${tokenInfo?.access_token}`,
-          Accept:'Application/json'
+  const GetUserProfile = (tokenInfo) => {
+    console.log("token info", tokenInfo);
+
+    axios
+      .get(
+        `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${tokenInfo?.access_token}`,
+        {
+          headers: {
+            Authorization: `Bearer ${tokenInfo?.access_token}`,
+            Accept: "Application/json",
+          },
         }
-  
-      }).then((res)=>{
-        console.log("response=",res);
-        localStorage.setItem('user',JSON.stringify(res.data))
+      )
+      .then((res) => {
+        console.log("response=", res);
+        localStorage.setItem("user", JSON.stringify(res.data));
         setOpenDailog(false);
-        window.location.reload()
-        
-      }).catch((err)=>{
-        console.error("error fetching user profile:",err);   
+        window.location.reload();
       })
-    }
-  const navigate= useNavigate();
-  function goToHomePage(){
-    navigate('/')
+      .catch((err) => {
+        console.error("error fetching user profile:", err);
+      });
+  };
+  const navigate = useNavigate();
+  function goToHomePage() {
+    navigate("/");
   }
 
   return (
     <div className="p-1 border-rounded flex h-16  w-full justify-between ">
-      <img src="image.png" alt="travel logo"/>
+      <img src="image.png" alt="travel logo" />
 
-      
       <div className="flex items-center gap-6">
-      <button className="text-white font-semibold" onClick={()=>{
-        goToHomePage();
-      }} >< HiHome className="size-6 "/> </button>
+        <button
+          className="text-white font-semibold"
+          onClick={() => {
+            goToHomePage();
+          }}
+        >
+          <HiHome className="size-6 " />{" "}
+        </button>
 
-        {user? (
-            <button onClick={()=>{
-                googleLogout(); 
-                setUser(null)
-                localStorage.removeItem("user");
-                navigate('/');
-                
-            }} className="bg-black text-white font-semibold p-2 rounded h-10">Log Out</button>
-        ):(
-            <button onClick={()=> setOpenDailog(true)} className="bg-black text-white font-semibold p-2 rounded h-10">Sign in </button>
+        {user ? (
+          <button
+            onClick={() => {
+              googleLogout();
+              setUser(null);
+              localStorage.removeItem("user");
+              navigate("/");
+            }}
+            className="bg-black text-white font-semibold p-2 rounded h-10"
+          >
+            Log Out
+          </button>
+        ) : (
+          <button
+            onClick={() => setOpenDailog(true)}
+            className="bg-black text-white font-semibold p-2 rounded h-10"
+          >
+            Sign in{" "}
+          </button>
         )}
       </div>
       <Dialog open={openDailog}>
-              
-              <DialogContent>
-                <DialogHeader>
-                  <DialogTitle>welcome</DialogTitle>
-                 
-                  <DialogDescription>
-                    <h2 className="font-bold text-lg mt-1">Sign with Google</h2>
-                    <p>Sign in with Google Authentication securely</p>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>welcome</DialogTitle>
 
-                    <Button 
-                    
-                    onClick={login}
-                    className="pt-1 w-full mt-4 flex"> 
-                    <FcGoogle className="h-8 w-8" />
-                    Sign in with Google</Button>
-                  </DialogDescription>
-                </DialogHeader>
-              </DialogContent>
-            </Dialog>
+            <DialogDescription>
+              <h2 className="font-bold text-lg mt-1">Sign with Google</h2>
+              <p>Sign in with Google Authentication securely</p>
+
+              <Button onClick={login} className="pt-1 w-full mt-4 flex">
+                <FcGoogle className="h-8 w-8" />
+                Sign in with Google
+              </Button>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
